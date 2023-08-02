6bc2702b314b886e94e4f559ef7ec6630cfd42f3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-08-01 17:03:17 -0700

    add post `/admin/background` endpoint

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,23 +2,25 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[admin_token]/cards/[card_id]/delete.tsx";
-import * as $1 from "./routes/[admin_token]/cards/index.tsx";
-import * as $2 from "./routes/[admin_token]/index.tsx";
-import * as $3 from "./routes/[admin_token]/title.tsx";
-import * as $4 from "./routes/_404.tsx";
-import * as $5 from "./routes/_app.tsx";
-import * as $6 from "./routes/index.tsx";
+import * as $0 from "./routes/[admin_token]/background.tsx";
+import * as $1 from "./routes/[admin_token]/cards/[card_id]/delete.tsx";
+import * as $2 from "./routes/[admin_token]/cards/index.tsx";
+import * as $3 from "./routes/[admin_token]/index.tsx";
+import * as $4 from "./routes/[admin_token]/title.tsx";
+import * as $5 from "./routes/_404.tsx";
+import * as $6 from "./routes/_app.tsx";
+import * as $7 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[admin_token]/cards/[card_id]/delete.tsx": $0,
-    "./routes/[admin_token]/cards/index.tsx": $1,
-    "./routes/[admin_token]/index.tsx": $2,
-    "./routes/[admin_token]/title.tsx": $3,
-    "./routes/_404.tsx": $4,
-    "./routes/_app.tsx": $5,
-    "./routes/index.tsx": $6,
+    "./routes/[admin_token]/background.tsx": $0,
+    "./routes/[admin_token]/cards/[card_id]/delete.tsx": $1,
+    "./routes/[admin_token]/cards/index.tsx": $2,
+    "./routes/[admin_token]/index.tsx": $3,
+    "./routes/[admin_token]/title.tsx": $4,
+    "./routes/_404.tsx": $5,
+    "./routes/_app.tsx": $6,
+    "./routes/index.tsx": $7,
   },
   islands: {},
   baseUrl: import.meta.url,
